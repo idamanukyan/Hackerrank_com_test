123e55deb6d2c673f40d33040d3e267aa6cb2177
idamanukyan <ida_manukyan@edu.aua.am>
2023-04-29 21:06:52 +0400

    Added remote web driver

--- src/test/java/org/example/pages/BaseTest.java
+++ src/test/java/org/example/pages/BaseTest.java
@@ -1,11 +1,18 @@
 package org.example.pages;
 
 import org.example.constants.url.Links;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
@@ -13,12 +20,19 @@ public class BaseTest {
     protected LoginPage loginPage;
 
     @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/main/java/org/example/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    public void setUp() throws MalformedURLException {
+        String hubUrl = "http://localhost:4444/grid/";
+        Capabilities capabilities = getCapabilities();
+        driver = new RemoteWebDriver(new URL(hubUrl), capabilities);
+        loginPage = new LoginPage(driver);
         moveToSignInPage();
+    }
 
+    private Capabilities getCapabilities() {
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+        options.setCapability(CapabilityType.PLATFORM_NAME, Platform.WINDOWS);
+        return options;
     }
 
     @BeforeMethod
